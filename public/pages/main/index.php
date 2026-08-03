95f34013c56de8dc86ac7ff538a689adb2dea7ac
TimQwees <artemnersisyan777@gmail.com>
2026-08-03 08:46:03 +0400

    update 3.0.0

--- public/pages/main/index.php
+++ public/pages/main/index.php
@@ -252,17 +252,17 @@ if (!in_array($activeSection, ['main', 'profile', 'setting', 'referal'], true)) 
                                 <!-- Monoblock decorative elements -->
                                 <div class="relative flex justify-center items-center flex-col w-1/3">
                                   <!-- bg -->
-                                    <img decoding="async" loading="lazy"
+                                    <img decoding="async"
                                         src="<?= $site['baseUrl'] ?>/public/assets/images/icons/services/monoblock/<?= htmlspecialchars($formattedVpnStatus['monoblock_image']['layout_bg']) ?>"
                                         alt="monoblock_top" title="monoblock_top" loading="lazy"
                                         class="z-20 w-full absolute z-10">
                                   <!-- spin -->
-                                    <img decoding="async" loading="lazy"
+                                    <img decoding="async"
                                         src="<?= $site['baseUrl'] ?>/public/assets/images/icons/services/monoblock/<?= htmlspecialchars($formattedVpnStatus['monoblock_image']['layout_spin']) ?>"
                                         alt="monoblock_down" title="monoblock_down" loading="lazy"
                                         class="z-10 w-[70%] absolute z-20 animate-spin [animation-duration:10s]">
                                   <!-- center -->
-                                    <img decoding="async" loading="lazy"
+                                    <img decoding="async"
                                         src="<?= $site['baseUrl'] ?>/public/assets/images/icons/services/monoblock/<?= htmlspecialchars($formattedVpnStatus['monoblock_image']['layout_center']) ?>"
                                         alt="monoblock_down" title="monoblock_down" loading="lazy"
                                         class="z-10 w-[35%] absolute z-30">
@@ -882,12 +882,12 @@ if (!in_array($activeSection, ['main', 'profile', 'setting', 'referal'], true)) 
                       <!-- spin -->
                         <img decoding="async" loading="lazy"
                             src="<?= $site['baseUrl'] ?>/public/assets/images/icons/services/monoblock/<?= htmlspecialchars($formattedVpnStatus['monoblock_image']['layout_spin']) ?>"
-                            alt="monoblock_down" title="monoblock_down" loading="lazy"
+                            alt="monoblock_down" title="monoblock_down"
                             class="z-10 w-[50%] absolute z-20 animate-spin [animation-duration:10s]">
                       <!-- center -->
-                        <img decoding="async" loading="lazy"
+                        <img
                             src="<?= $site['baseUrl'] ?>/public/assets/images/icons/services/monoblock/<?= htmlspecialchars($formattedVpnStatus['monoblock_image']['layout_center']) ?>"
-                            alt="monoblock_down" title="monoblock_down" loading="lazy"
+                            alt="monoblock_down" title="monoblock_down"
                             class="z-10 w-[22%] absolute z-30">
                     </div>
 
